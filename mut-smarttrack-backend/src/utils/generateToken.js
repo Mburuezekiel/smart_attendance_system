93b3a7ce0008bc452bedebbd3d3d2a31c3e0eba3
Mburuezekiel <mburuezekiel42@gmail.com>
2026-03-18 09:54:40 +0300

    updated assignments

--- mut-smarttrack-backend/src/utils/generateToken.js
+++ mut-smarttrack-backend/src/utils/generateToken.js
@@ -2,5 +2,5 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET, JWT_EXPIRES_IN } from '../config/env.js';
 
 export const generateToken = (userId, role) => {
-  return jwt.sign({ id: userId, role }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+  return jwt.sign({ _id: userId, role }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 };
