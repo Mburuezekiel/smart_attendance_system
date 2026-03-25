79de7b82b1a755331f32c21269f12f92eaac82fd
Mburuezekiel <mburuezekiel42@gmail.com>
2026-03-25 08:25:50 +0300

    changes

--- mut-smarttrack-backend/server.js
+++ mut-smarttrack-backend/server.js
@@ -5,5 +5,5 @@ import { PORT } from './src/config/env.js';
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Connection Successful the  Server running on port ${PORT}`);
 });
